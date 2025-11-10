0a764c13ac40cfecc3c3ad2fee279608a698906d
thanhnguyn1304-cmyk <thanhnguyn1304@gmail.com>
2025-11-11 00:27:19 +0700

    Initial commit : backend with FastAPI and SQLModel

---
+++ main.py
@@ -0,0 +1,104 @@
+import requests, uvicorn
+from fastapi import FastAPI
+from sqlmodel import SQLModel, Field, create_engine, Session, select, delete 
+import json
+import os
+
+url = "https://valorant-api.com/v1/agents"
+response = requests.get(url)
+data = response.json()
+print(response.status_code)
+
+
+# 200 is ok, 404 = not found, 403/401 = forbidden/ unauthorized
+
+# print(response.text) to see whats really in there sometimes 200 but the actual message was 'rate limit exceeded' that won't work
+
+# print(data['data'][0]['displayName']) prints out Gekko
+
+# 1. create database
+app = FastAPI()
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+database_connection_string = f"postgresql://postgres:{DB_PASSWORD}@localhost:5432/valorant_db"
+
+
+engine = create_engine(database_connection_string, echo = True) 
+
+class Agent(SQLModel, table = True):
+    id: int | None = Field(default = None, primary_key = True)
+    displayName : str
+    role : str
+    description : str
+
+@app.on_event("startup")
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+#2. we do the updating part
+
+@app.get("/agents")
+def get_all_agents():
+    
+    #2.1 returning db
+
+    with Session(engine) as session: #Session is the shopping cart, use session.add() and session.commit() to checkout
+        statement = select(Agent) # build the query, checking the table we created above
+        results = session.exec(statement) # run the query
+        agents_in_db = results.all() #returning what have been found
+        
+        if agents_in_db :
+            print('Data found in pantry. Returning fast')
+            return agents_in_db
+        
+        #2.2 if db is empty, go take it from website
+        
+        else:
+            print('Pantry empty. Going to grocery store (API)...')
+            
+
+
+            response = requests.get(url)
+            data = response.json()
+            
+            #for agent_json in data['data']:
+
+            # first agent checkng raw json using print(json.dumps(...))
+
+            # --- Our New Debug Code ---
+            #first_agent_data = data['data'][0] 
+            
+            #print("--- First Agent Data ---")
+            #print(json.dumps(first_agent_data, indent=4))
+            
+            #print("------------------------")
+
+            
+            agents_to_add = [] #adding agents now from the url as we didnt find in db earlier
+
+            for agent_json in data['data']:
+                new_agent = Agent(
+                displayName=agent_json['displayName'],
+                role=agent_json['role']['displayName'],
+                description=agent_json['description'],
+                )
+                session.add(new_agent)
+                agents_to_add.append(new_agent)
+            
+            session.commit()
+            
+            for agent in agents_to_add:
+                session.refresh(agent)
+            return agents_to_add
+
+#done with the update part in CRUD, now we go to the delete part
+
+@app.post("/refresh-agents")
+def refresh_agents_cache():
+    with Session(engine) as session:
+        statement = delete(Agent) 
+        #deleting everything, can add where(Agent.displayName == "Jett")), delete(Agent)
+        session.exec(statement)
+        session.commit()
+        return 'Cache is cleared. Pantry is now empty'
