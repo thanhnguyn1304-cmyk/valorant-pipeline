19923c0ead421a678dc5a466e1570d9bee8c72e5
thanhnguyn1304-cmyk <thanhnguyn1304@gmail.com>
2026-01-02 23:02:46 +0700

    Added a newmodel, writing up code for saving matches info to db

--- models/match.py
+++ models/match.py
@@ -1,17 +1,37 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
 
 
 class Match(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    agentName: str
-    mapName: str
-    score: str
-    roundsWon: str
-    roundsLost: str
-    kda: str
-    kdRatio: str
-    isWin: bool
-    position: str
-    hsPercent: int
-    adr: int
-    acs: int
+    id: str = Field(primary_key=True)  # The Riot Match ID (e.g., "a7f3...")
+    map_name: str
+    start_time: int  # Unix timestamp (e.g., 1703650000)
+    duration_ms: int  # Duration in milliseconds
+    winning_team: str  # "Blue" or "Red"
+    rounds_play : int
+    # THE RELATIONSHIP:
+    # This says: "I have a list of players. Go look at the 'match' variable in the other table."
+    participations: List["MatchParticipation"] = Relationship(back_populates="match")
+
+
+class MatchParticipation(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    match_id: str = Field(foreign_key="match.id")
+
+    rounds_played: int = Field(foreign_key="match.rounds_play")
+
+    puuid: str
+    agent_name: str
+    team_id: str
+    current_rank:str
+    kills: int
+    deaths: int
+    assists: int
+    acs : int
+    headshots: int
+    othershots: int
+
+    damage_dealt: int
+    damage_taken: int
+
+    match: Optional[Match] = Relationship(back_populates="participations")
--- services/match_service.py
+++ services/match_service.py
@@ -1,6 +1,9 @@
 import httpx
 from backend.core.config import settings
 from fastapi import HTTPException
+from backend.models.match import Match, MatchParticipation
+from sqlmodel import Session, select
+
 
 
 class MatchService:
@@ -10,12 +13,46 @@ class MatchService:
         self.client = httpx.AsyncClient(timeout=httpx.Timeout(10.0))
 
     async def get_matches_by_region_and_puuid(self, region: str, puuid: str):
-        url = self.base_url + f"/v3/by-puuid/matches/{region}/{puuid}?mode=competitive&size=5"
+        url = self.base_url + f"/v3/by-puuid/matches/{region}/{puuid}?mode=competitive&size=2"
         async with self.client as client:
             response = await client.get(url, headers=self.headers)
             if response.status_code == 200:
                 data = response.json()
-                return data["data"]
+                match_data = data['data']
+                return match_data
+
+
             error_msg = f"HenrikAPI Error: {response.status_code} - {response.text}"
             print(error_msg)  # In ra terminal (phòng hờ)
             raise HTTPException(status_code=response.status_code, detail=error_msg)
+        
+    async def fetch_and_update_matches(self, match_data, puuid, db: Session):
+        for match_json in match_data:
+            match_id = match_json["metadata"]["matchId"]
+            statement = select(Match).where(Match.id == match_id)
+            match_in_db = db.exec(statement).first()
+            if match_in_db :
+                continue
+            else:
+                new_match = Match(
+                    id=match_id,
+                    map_name=match_json["metadata"]["map"],
+                    start_time=match_json["metadata"]["game_start_patched"],
+                    duration_ms=match_json["metadata"]["game_length"],
+                    winning_team = 'red' if match_json["teams"]["red"]["has_won"] else 'blue', # "Blue" or "Red"
+                    rounds_play = match_json["metadata"]["rounds_played"]
+
+
+
+
+
+
+
+        matches_in_db = db.exec(select(Match)).all()
+        
+        for match in match_data:
+
+            
+            match_id = match["metadata"]["matchId"]
+
+        
